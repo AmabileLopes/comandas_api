9572903d8e099c0c92f4ba7a8a7f4cc23c22401b
Amabile <amabilelopesouriques@gmail.com>
2026-03-09 17:47:40 -0300

    entrega 2

---
+++ src/domain/Entities/Produto.py
@@ -0,0 +1,10 @@
+from pydantic import BaseModel
+
+class Produto(BaseModel):
+    id_produto: int = None
+    nome: str
+    descricao: str
+    foto: bytes
+    valor_unitario: float
+
+    ##Amabile Vitória Lopes Ouriques
